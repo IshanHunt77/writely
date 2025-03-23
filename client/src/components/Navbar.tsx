54a7d4c7347ace34ead0e65e6c761b5d78d2950b
IshanHunt77 <143602491+IshanHunt77@users.noreply.github.com>
2025-03-23 22:33:48 +0530

    Update Navbar.tsx

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,30 +1,28 @@
-import React from "react";
 import { useNavigate } from "react-router-dom";
 import { CgProfile } from "react-icons/cg";
-import DarkModeIcon from '@mui/icons-material/DarkMode';
 import { FaHome } from "react-icons/fa";
-import ExploreIcon from '@mui/icons-material/Explore';
 import { useRecoilValue } from "recoil";
 import { usernameatom } from "../atoms/usernameatom";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import ExploreIcon from "@mui/icons-material/Explore";
 
 const Navbar: React.FC = () => {
   const navigate = useNavigate();
   const user = useRecoilValue(usernameatom);
-  const username =
-    typeof user === "object" && user !== null ? user.username || "" : user;
+  const username = typeof user === "object" && user?.username ? user.username : "";
 
   return (
     <nav className="bg-amber-900 text-white p-4 flex justify-end items-center rounded-lg">
       <button
         title="Home"
-        onClick={() => navigate(`/${username}/blogs`)}
+        onClick={() => navigate(username ? `/${username}/blogs` : "/")}
         className="hover:text-gray-300 mr-8 rounded-lg"
       >
         <FaHome />
       </button>
       <button
         title="Explore"
-        onClick={() => navigate(`/${username}/allblogs`)}
+        onClick={() => navigate(username ? `/${username}/allblogs` : "/")}
         className="hover:text-gray-300 mr-8 rounded-lg"
       >
         <ExploreIcon />
@@ -38,7 +36,7 @@ const Navbar: React.FC = () => {
       </button>
       <button
         title="Profile"
-        onClick={() => navigate(`/${username}/profile`)}
+        onClick={() => navigate(username ? `/${username}/profile` : "/")}
         className="hover:text-gray-300"
       >
         <CgProfile />
